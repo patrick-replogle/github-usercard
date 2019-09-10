96160e6f1c8f2323517f705e676b1a2a28c5b7b4
patrick-replogle <patrickr1138@gmail.com>
2019-09-09 19:15:43 -0700

    project setup and function started.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,15 @@
            https://api.github.com/users/<your name>
 */
 
+axios.get('https://api.github.com/users/patrick-replogle')
+  .then((response) => {
+    console.log(response)
+  })
+  .catch((error) => {
+    console.log(error);
+    console.log('there was an error')
+  })
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -13,6 +22,8 @@
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
+
+
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -53,3 +64,54 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+function cardMaker(data) {
+  let card = document.createElement('div')
+  card.classList.add('card')
+
+  let img = document.createElement('img')
+  img.setAttribute(src, data.avatar_url)
+  card.appendChild(img)
+
+  let div = document.createElement('div')
+  div.classList.add('card-info')
+  card.appendChild(div)
+
+  let h3 = document.createElement('h3')
+  h3.classList.add('name')
+  h3.textContent = `${data.name}`
+  div.appendChild(h3)
+
+  let p1 = document.createElement('p')
+  p1.classList.add('username')
+  p1.textContent = `${data.login}`
+  div.appendChild(p1)
+
+  let p2 = document.createElement('p')
+  p2.textContent = `Location: ${data.location}`
+  div.appendChild(p2)
+
+  let p3 = document.createElement('p')
+  p3.textContent = `Profile: `
+  let a = document.createElement('a');
+  a.setAttribute(href, data.html_url)
+  a.setAttribute(alt, 'address to user\'s github page')
+  p3.appendChild('a')
+  div.appendChild(p3);
+
+  let p4 = document.createElement('p')
+  p4.textContent = `Followers: ${data.followers}`
+  div.appendChild(p4)
+
+  let p5 = document.createElement('p')
+  p5.textContent = `Following: ${data.following}`
+  div.appendChild(p5)
+
+  let p6 = document.createElement('p')
+  p6.textContent = `Bio: ${data.bio}`
+  div.appendChild(p6)
+
+  return card;
+
+}
+console.log(cardMaker)
